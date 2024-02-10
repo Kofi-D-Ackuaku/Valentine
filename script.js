6f48e75a637ad083a6bf9a3514fbcd2d39a1a65c
kofi-D-Ackuaku <kackuakuk@gmail.com>
2024-02-10 16:42:57 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+function showMessage(response) {
+  if (response === "No") {
+    const noButton = document.getElementById("no-button");
+    const container = document.querySelector(".container");
+    const maxWidth = window.innerWidth - noButton.offsetWidth;
+    const maxHeight = window.innerHeight - noButton.offsetHeight;
+
+    
+    noButton.style.position = "absolute";
+
+    
+    document.getElementsByClassName("image")[0].src = "8fczav.jpg";
+
+    
+    const randomX = Math.max(0, Math.floor(Math.random() * maxWidth));
+    const randomY = Math.max(0, Math.floor(Math.random() * maxHeight));
+
+    
+    noButton.style.left = randomX + "px";
+    noButton.style.top = randomY + "px";
+
+    
+    document.getElementById("question").textContent =
+      "Can't believe you chose no but the no button is just for visuals";
+    document.getElementById("name").style.display = "none";
+
+    
+  }
+
+  if (response === "Yes") {
+    
+    document.getElementById("name").remove();
+    document.getElementById("no-button").remove();
+
+    
+    const yesMessage = document.getElementById("question");
+    yesMessage.textContent = "LESGOOO see you on the 14th babe😘😘";
+    yesMessage.style.display = "block";
+    yesMessage.style.fontStyle = "normal";
+    document.getElementsByClassName("image")[0].src = "giphy.gif";
+
+    
+    document.getElementById("yesButton").remove();
+  }
+}
